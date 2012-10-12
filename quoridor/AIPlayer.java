e7e509a6a84d22cb1088c7c00f71c5d044033fad
Louis Tiao <louistiao@gmail.com>
2012-10-13 01:29:04 +1100

    some alpha beta pruning and negamax happening here

--- quoridor/AIPlayer.java
+++ quoridor/AIPlayer.java
@@ -1,15 +1,25 @@
 package quoridor;
 
 import java.util.List;
+import static java.util.Arrays.asList;
 
 public class AIPlayer implements Player {
 
+	List <String> shiller = asList("e8", "e2", "e7", "e3", "e6", "e4", "c8v");
+	List <String> ala = asList("e8", "e2", "e7", "e3", "e6", "e4", "d6h", "f6h", "c5v", "g5v");
+
+	
 	@Override
 	public String getMove(GameState gs) {
 		List<String> validMoves = gs.validMoves();
 		//System.out.println(validMoves);
-		System.out.println(minimax (gs, 3));
-		return validMoves.get((int)(Math.random() * (validMoves.size() + 1)));
+		if (gs.turn() < 7) {
+			System.out.println(shiller);
+			return shiller.get(gs.turn());
+		} else {
+			System.out.println(negamax(gs, 4, Integer.MIN_VALUE, Integer.MAX_VALUE, 1));	
+			return validMoves.get((int)(Math.random() * (validMoves.size() + 1)));
+		}
 	}
 
 	public int minimax (GameState gs, int depth) {
@@ -23,6 +33,29 @@ public class AIPlayer implements Player {
 			a = Math.max(a, -minimax(child, depth-1));
 		}
 		return a;
+	}
+	
+	public int negamax(GameState node, int depth, int alpha, int beta, int color) {
+		System.out.println(node);
+		if (node.isOver() || depth == 0) {
+			int temp = color * node.heuristic();
+			System.out.println(temp);
+			return temp;
+		} else {
+			for (String e:node.validMoves()) {
+				GameState child = new GameState(node);
+				child.move(e);
+				int val = -negamax( child, depth-1, -beta, -alpha, -color);
+				if (val >= beta) {
+					return val;
+				}
+				if (val >= alpha) {
+					alpha = val;
+				}
+			}
+			System.out.println(alpha);
+			return alpha;
+		}
 	}
 	
 }
