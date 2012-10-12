a0c9028d7eea69f6d6a34679eb0ec4ad27cc565e
jloong <onphire6277@gmail.com>
2012-10-13 02:28:43 +1200

    Update quoridor/UserInterface.java

--- quoridor/UserInterface.java
+++ quoridor/UserInterface.java
@@ -1,13 +1,18 @@
 package quoridor;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class UserInterface {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String menu = "********MENU********\n***Options***\n1:Human vs Human\t2:Human vs AI";
 		System.out.print(menu);
 		Boolean validInput = false;
@@ -19,10 +24,33 @@ public class UserInterface {
 			command = in.nextLine();
 			if (command.equals("1")) {
 				validInput = true;
+				player1 = new HumanPlayer();
+				player2 = new HumanPlayer();
 			}
 			else if (command.equals("2")) {
 				validInput = true;
+				player1 = new HumanPlayer();
 				player2 = new AIPlayer();
+			}
+			else if (command.equals("3")) {
+				System.out.println("Enter file name:");
+				String FileName = null;
+				Scanner fileIn = new Scanner(System.in);
+				FileName = fileIn.nextLine();
+				BufferedReader br = new BufferedReader(new FileReader(FileName));
+				try {
+					StringBuilder sb = new StringBuilder();
+					String line = br.readLine();
+					
+					while (line != null) {
+						sb.append(line);
+						sb.append("\n");
+						line = br.readLine();
+					}
+					String gameString = sb.toString();
+				} finally {
+					br.close();
+				}
 			}
 			else {
 				System.out.print("Invalid selection\nInput selection:");
