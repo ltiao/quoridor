a62cf9cd198cbaf65694d896fde6b74534387330
jloong <onphire6277@gmail.com>
2012-10-13 01:05:08 +1200

    Update quoridor/UserInterface.java

--- quoridor/UserInterface.java
+++ quoridor/UserInterface.java
@@ -1,13 +1,36 @@
 package quoridor;
 
+import java.util.Scanner;
+
 public class UserInterface {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		String menu = "********MENU********\n***Options***\n1:Human vs Human\t2:Human vs AI";
+		System.out.print(menu);
+		Boolean validInput = false;
 		Player player1 = new HumanPlayer();
-		Player player2 = new AIPlayer();
+		Player player2 = new HumanPlayer();
+		while (!validInput) { 
+			String command = null;
+			Scanner in = new Scanner(System.in);
+			command = in.nextLine();
+			if (command.equals("1")) {
+				validInput = true;
+				player1 = new HumanPlayer();
+				player2 = new HumanPlayer();
+			}
+			else if (command.equals("2")) {
+				validInput = true;
+				player1 = new HumanPlayer();
+				player2 = new AIPlayer();
+			}
+			else {
+				System.out.print("Invalid selection\nInput selection:");
+			}
+		}
 		String move = new String();
 		GameState gs = new GameState();
 		while(!gs.isOver()) {
